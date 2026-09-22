83ccbffc5c83256766ec765f6255344b6c57a4fe
Master <faizkhan.dev35@gmail.com>
2026-09-22 17:15:01 +0530

    Fix config for deployment

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_KEY = os.environ.get("GEMINI_API_KEY", "")
+MODEL = "gemini-3-flash"
+
+YOUR_NAME = "master"
+AI_NAME = "Lisa"
+APP_PASSWORD = os.environ.get("APP_PASSWORD", "changeme")
